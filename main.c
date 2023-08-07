0c1d33f02983a82b7a6fa72ab585a8b6da3e6a00
alejandrothinkion <89306401+sqlgengar@users.noreply.github.com>
2023-08-07 05:12:01 -0300

    Agregar click derecho

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main()
 {
-    printf( "Soy una prueba de compilacion :D \n" );
+    // Obtiene las coordenadas actuales del cursor
+    POINT cursorPosition;
+    GetCursorPos(&cursorPosition);
+
+    // Realiza un clic del botón izquierdo del mouse en las coordenadas actuales
+    mouse_event(MOUSEEVENTF_RIGHTDOWN, cursorPosition.x, cursorPosition.y, 0, 0);
+    mouse_event(MOUSEEVENTF_RIGHTUP, cursorPosition.x, cursorPosition.y, 0, 0);
+
     return 0;
 }
